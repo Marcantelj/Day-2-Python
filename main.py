57e0ea24a89614764f0b97ee0918c0e6516a2d91
Jonathan Marcantel <54511636+Marcantelj@users.noreply.github.com>
2024-05-14 20:15:47 -0500

    Update main.py
    Added a few comments

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-total_bill = float(input("What was the total bill? $"))
-tip_percentage = int(input("What percentage tip would? Just the number not % needed. "))
-people = int(input("How many people to split the bill? Make sure to count yourself! "))
-tip_percentage = tip_percentage / 100
-tip_amount = total_bill * tip_percentage
-print("Tip amount is: $" + str(tip_amount))
-total_bill = total_bill + tip_amount
+total_bill = float(input("What was the total bill? $")) # Collect Total Bill in US Dollars
+tip_percentage = int(input("What percentage tip would? Just the number not % needed. ")) # Collect Tip %
+people = int(input("How many people to split the bill? Make sure to count yourself! ")) # Collect amount of people to split bill
+tip_percentage = tip_percentage / 100 # Calculation of % 
+tip_amount = total_bill * tip_percentage # Calculation of tip amount to add to total bill 
+print("Tip amount is: $" + str(tip_amount)) 
+total_bill = total_bill + tip_amount # Sum of total bill and tip amount
 print("Total bill amount is: $" + str(total_bill))
-split_bill = total_bill / people
+split_bill = total_bill / people # finding the split bill amount
 print("Each person should pay: $" + str(split_bill))
